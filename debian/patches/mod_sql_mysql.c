f47f3656b6e47e91e47eda67aa6246aa53ed9b92
Francesco P. Lovergine <frankie@debian.org>
2011-02-08 13:47:58 +0100

    New version 1.3.3d with 3.0 format

---
+++ debian/patches/mod_sql_mysql.c
@@ -0,0 +1,13 @@
+Index: proftpd-dfsg/contrib/mod_sql_mysql.c
+===================================================================
+--- proftpd-dfsg.orig/contrib/mod_sql_mysql.c	2011-02-07 17:47:07.000000000 +0100
++++ proftpd-dfsg/contrib/mod_sql_mysql.c	2011-02-08 13:35:03.000000000 +0100
+@@ -135,7 +135,7 @@
+ #include "conf.h"
+ #include "../contrib/mod_sql.h"
+ 
+-#include <mysql.h>
++#include <mysql/mysql.h>
+ 
+ /* 
+  * timer-handling code adds the need for a couple of forward declarations
