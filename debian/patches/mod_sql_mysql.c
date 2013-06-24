e4fd614e8145b63a4af2cb56611570139e4345e0
Francesco Paolo Lovergine <frankie@debian.org>
2013-06-24 17:58:28 +0200

    Refreshed one more patch.

--- debian/patches/mod_sql_mysql.c
+++ debian/patches/mod_sql_mysql.c
@@ -4,8 +4,8 @@ Forwarded: not needed
 
 Index: proftpd-dfsg/contrib/mod_sql_mysql.c
 ===================================================================
---- proftpd-dfsg.orig/contrib/mod_sql_mysql.c	2011-02-07 17:47:07.000000000 +0100
-+++ proftpd-dfsg/contrib/mod_sql_mysql.c	2011-02-08 13:35:03.000000000 +0100
+--- proftpd-dfsg.orig/contrib/mod_sql_mysql.c	2013-06-24 17:58:16.000000000 +0200
++++ proftpd-dfsg/contrib/mod_sql_mysql.c	2013-06-24 17:58:16.000000000 +0200
 @@ -135,7 +135,7 @@
  #include "conf.h"
  #include "../contrib/mod_sql.h"
@@ -13,5 +13,5 @@ Index: proftpd-dfsg/contrib/mod_sql_mysql.c
 -#include <mysql.h>
 +#include <mysql/mysql.h>
  
- /* 
-  * timer-handling code adds the need for a couple of forward declarations
+ /* The my_make_scrambled_password{,_323} functions are not part of the public
+  * MySQL API and are not declared in any of the MySQL header files. But the
