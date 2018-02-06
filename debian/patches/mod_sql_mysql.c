dcf153e7960fc842554567d311d1391567fca036
Francesco Paolo Lovergine <frankie@debian.org>
2018-02-06 15:03:19 +0100

    Patches refreshd

--- debian/patches/mod_sql_mysql.c
+++ debian/patches/mod_sql_mysql.c
@@ -2,11 +2,9 @@ Description: Use mysql/mysql.h instead of mysql.h
 Author: Francesco Paolo Lovergine <frankie@debian.org>
 Forwarded: not needed
 
-Index: git/contrib/mod_sql_mysql.c
-===================================================================
---- git.orig/contrib/mod_sql_mysql.c	2016-12-10 20:51:54.996366780 +0100
-+++ git/contrib/mod_sql_mysql.c	2016-12-10 20:51:54.988366803 +0100
-@@ -132,7 +132,7 @@
+--- proftpd-dfsg.orig/contrib/mod_sql_mysql.c
++++ proftpd-dfsg/contrib/mod_sql_mysql.c
+@@ -131,7 +131,7 @@
  #include "conf.h"
  #include "../contrib/mod_sql.h"
  
